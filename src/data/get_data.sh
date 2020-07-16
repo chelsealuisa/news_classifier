f71f9f2798e321a7fbe29a41f3735b02b5c52357
chelsea <chelsealuisa@hotmail.co.uk>
2020-07-16 15:40:13 +0200

    Add script to get data

---
+++ src/data/get_data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ../data/raw/
+wget https://archive.ics.uci.edu/ml/machine-learning-databases/00359/NewsAggregatorDataset.zip
+unzip NewsAggregatorDataset.zip
+rm NewsAggregatorDataset.zip
